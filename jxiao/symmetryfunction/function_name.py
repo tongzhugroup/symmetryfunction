b6c15a8474874c925823f4753f1a4bedc0448e5f
Jin Xiao <ninggujxiao@Gmail.com>
2022-09-24 17:18:33 +0800

    update angular function

--- jxiao/symmetryfunction/function_name.py
+++ jxiao/symmetryfunction/function_name.py
@@ -11,6 +11,16 @@ def symm_f2(atom_i: np.ndarray, atom_j: np.ndarray, eta: float = 1, R_s: float =
     R_ij = np.linalg.norm(a-b)
     return np.exp(-eta * (R_ij-R_s)**2)
 
+def symm_f4(atom_i: np.ndarray, atom_j: np.ndarray, atom_k: np.ndarray, eta: float = 1, lamdb: float = 1, zeta: float = 1):
+    a=np.array([float(i) for i in atom_i])
+    b=np.array([float(j) for j in atom_j])
+    c=np.array([float(k) for k in atom_k])
+    R_ij = np.linalg.norm(a-b)
+    R_ik = np.linalg.norm(a-c)
+    R_jk = np.linalg.norm(b-c)
+    theta_ijk = np.arccos((R_ij**2 + R_ik**2 - R_jk**2) / (2 * R_ij * R_ik))
+    return (1 + lamdb * np.cos(theta_ijk))**zeta * np.exp(-eta * R_ij**2+R_ik**2+R_jk**2)
+
 def cutoff(atom_i: np.ndarray, atom_j: np.ndarray, R_c: float=None) -> float:
     a=np.array([float(i) for i in atom_i])
     b=np.array([float(j) for j in atom_j])
@@ -25,12 +35,21 @@ def cutoff(atom_i: np.ndarray, atom_j: np.ndarray, R_c: float=None) -> float:
     return f_c
 
 
-
-
 #  get eta and R_s couple
-def get_symm_params(eta_list: List = None , R_s_list: List=None) -> List[Tuple[float]]:
+def get_symm2_params(eta_list: List = None , R_s_list: List=None) -> List[Tuple[float]]:
     if not eta_list:
         eta_list=[1]
     if not R_s_list:
         R_s_list=[1]
     return list(itertools.product(eta_list, R_s_list))
+
+def get_symm4_params(eta_list: List = None , lamdb_list: List=None, zeta_list: List=None, R_s_list: List=None) -> List[Tuple[float]]:
+    if not eta_list:
+        eta_list=[1]
+    if not lamdb_list:
+        lamdb_list=[1]
+    if not zeta_list:
+        zeta_list=[1]
+    if not R_s_list:
+        R_s_list=[1]
+    return list(itertools.product(eta_list, lamdb_list, zeta_list, R_s_list))
--- jxiao/symmetryfunction/run.py
+++ jxiao/symmetryfunction/run.py
@@ -5,17 +5,11 @@ from function_name import *
 from typing import List, Tuple, Set,Dict
 
 
-R_c = 10
-eta=[1,2,3]
-R_s=[7,8]
-at_types=['C','H','N','O']
 
-mol = [['O', '-0.005703', '0.385159', '-0.000000'], 
-        ['H', '-0.796078', '-0.194675', '-0.000000'], 
-        ['H', '0.801781', '-0.190484', '0.000000']]
 
-def symm_f_mol(mol,atom_types, eta_list, R_s_list,R_c:float=None):
-    symm_f_params = get_symm_params(eta_list, R_s_list)
+
+def symm_f2_mol(mol,atom_types, eta_list, R_s_list, R_c:float=None):
+    symm_f_params = get_symm2_params(eta_list, R_s_list)
 
     pairwise_permutations = np.array(list(itertools.permutations(mol,2))) # get coord pair
     
@@ -25,18 +19,61 @@ def symm_f_mol(mol,atom_types, eta_list, R_s_list,R_c:float=None):
             if (pairwise_permutations[j][0] == mol[i]).all(): #'center atom vs. atom in xyz'
                 for p1,p2 in symm_f_params:
                     atom_dict = atom_list[i]
-
                     f_c = cutoff(pairwise_permutations[j][0][1:],pairwise_permutations[j][1][1:])
                     sf = symm_f2(pairwise_permutations[j][0][1:],
                                 pairwise_permutations[j][1][1:],
                                 eta=p1,R_s=p2) * f_c
-                    print(sf)
                     key = "-".join(pairwise_permutations[j][:,0])+": eta="+str(p1)+" R_s="+str(p2) #'long key name to check where values come from'
                     atom_dict[key] = atom_dict.setdefault(key,0) + sf
 
             else:
                 continue
     return atom_list
+mol = [['O', '-0.005703', '0.385159', '-0.000000'],
+        ['H', '-0.796078', '-0.194675', '-0.000000'], 
+        ['H', '0.801781', '-0.190484', '0.000000']]
 
-print(*symm_f_mol(mol, at_types, eta, R_s), sep ="\n")
+def symm_f4_mol(mol, atom_types, eta_list, lamdb_list, zeta_list, R_s_list):
+    threewise_permutations = np.array(list(itertools.permutations(mol,3)))
+    symm_f_params = get_symm4_params(eta_list, lamdb_list, zeta_list, R_s_list)
+    atom_list = [{} for i in range(len(mol))]
+    for i in range(len(atom_list)):
+        for j in range(len(threewise_permutations)):
+            if (threewise_permutations[j][0] == mol[i]).all():
+                for p1,p2,p3,p4 in symm_f_params:
+                    atom_dict = atom_list[i]
+                    f_c = cutoff(threewise_permutations[j][0][1:],threewise_permutations[j][1][1:])
+                    #print(threewise_permutations[j][0][1:],threewise_permutations[j][1][1:],threewise_permutations[j][2][1:])
+                    sf = symm_f4(threewise_permutations[j][0][1:],
+                                threewise_permutations[j][1][1:],
+                                threewise_permutations[j][2][1:],
+                                eta=p1, lamdb=p2, zeta=p3) * f_c
+
+                    key = "-".join(threewise_permutations[j][:,0])+": eta="+str(p1)+" lamda="+str(p2)+" zeta="+str(p3)+" R_s="+str(p4)
+                    atom_dict[key] = atom_dict.setdefault(key,0) + sf
+            else:
+                continue
+    return atom_list
+            
+R_c = 10
+eta=[1,2,3]
+lamda=[-1,1]
+zeta=[4,5,6]
+R_s=[7,8]
+at_types=['C','H','N','O']
+
+mol = [['O', '-0.005703', '0.385159', '-0.000000'],
+        ['H', '-0.796078', '-0.194675', '-0.000000'], 
+        ['H', '0.801781', '-0.190484', '0.000000']]
+
+for i in symm_f2_mol(mol,at_types,eta,R_s,R_c):
+    for j in i:
+        print(j, i[j])
+
+for i in symm_f4_mol(mol,at_types,eta,lamda,zeta,R_s):
+    for j in i:
+        print(j,i[j])
+
+#print(*symm_f2_mol(mol, at_types, eta, R_s), sep ="\n")
+#print(*symm_f4_mol(mol,at_types, eta, lamda, zeta, R_s),sep ="\n")
 
